04163794cc27c6fbfa22033087c83069f8ad2efd
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-09-25 16:47:17 -0500

    User Controls created in UI folder

---
+++ Related_and_Minimum_Automata/Related_and_Minimum_Automata/ui/MooreStates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Related_and_Minimum_Automata.ui
+{
+    public partial class MooreStates : UserControl
+    {
+        public MooreStates()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
