6c06cf301e0aafe097429761bdce76d8b601215b
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-09-29 16:45:51 -0500

    UI and model integration advance

--- Related_and_Minimum_Automata/Related_and_Minimum_Automata/ui/MainWindow.cs
+++ Related_and_Minimum_Automata/Related_and_Minimum_Automata/ui/MainWindow.cs
@@ -49,7 +49,9 @@ namespace Related_and_Minimum_Automata.ui
 
         private void ReduceMachineButton_Click(object sender, EventArgs e)
         {
-
+            controller.LoadMachine(machineTable1.GetRowsData()); 
+            //partition
+            //load DataGridView
         }
 
         private void BackButton_Click(object sender, EventArgs e)
