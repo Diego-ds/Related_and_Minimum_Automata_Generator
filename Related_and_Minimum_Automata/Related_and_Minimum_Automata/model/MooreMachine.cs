89ff6a28a780ee2c9d4f31430baf565e7245ef20
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2021-10-02 23:11:51 -0500

    Methods for machine partition implemented

--- Related_and_Minimum_Automata/Related_and_Minimum_Automata/model/MooreMachine.cs
+++ Related_and_Minimum_Automata/Related_and_Minimum_Automata/model/MooreMachine.cs
@@ -61,6 +61,121 @@ namespace Related_and_Minimum_Automata.model
 			States = connStates;
 		}
 
+		public void PartitionateMachine()
+        {
+			//List <MooreState> newPartition = Metodo de Diego 
+			List <MooreState> prevPartition = new List<MooreState>();
+
+			while (!PartitionsEquals(newPartition, prevPartition))
+            {
+				prevPartition = newPartition;
+				newPartition = NextPartition(prevPartition);
+            }
+		}
+
+		/*
+ * ALGORTIMO DE PARTICION:
+ * 1. Comparamos el primer estado de cada bloque con los otros estados de ese bloque
+ *	1.1 Si se cumple f(q,s) y f(q',s), no hacer nada
+ *	1.2 Si no se cumple, agregar el estado con que se esta comparando a un nuevo bloque D
+ * 2. Comparamos con el primer estado de la lista D el resto de estados
+ *	2.1 Si se cumple f(q,s) y f(q',s), no hacer nada
+ *	2.2 Si no se cumple, agregar el estado con que se esta comparando a un nuevo bloque D'
+ * 3. Si no resultan mas bloques D, agregar los bloques D a la particion y remover
+ * los estados que pertenecen a los bloques D del bloque anterior
+ * 4. Repetir hasta que se recorran todos los bloques de la particion
+ * 5. Retornar la particion con los bloques D agregados
+ */
+		public List<List<MooreState>> NextPartition(List<List<MooreState>> prevPartition)
+		{
+			List<List<MooreState>> newBlocks = new List<List<MooreState>>();
+
+			foreach (List<MooreState> block in prevPartition)
+			{
+				List<MooreState> differentStates = new List<MooreState>();
+
+				for (int i = 1; i < block.Count; i++)
+				{	
+					if (!TransitionFunctionEquals(block[0], block[i],prevPartition))
+					{
+						differentStates.Add(block[i]);
+					}
+				}
+
+				newBlocks.Add(differentStates);
+				foreach
+			}
+		}
+
+		public bool TransitionFunctionEquals(MooreState q1, MooreState q2, List<List<MooreState>> partition)
+        {
+			bool equal = true;
+			List<MooreState> ts1 = new List<MooreState>();
+			List<MooreState> ts2 = new List<MooreState>();
+
+			for (int i = 0; i < q1.Transitions.Count; i++)
+            {
+				ts1.Add(q1.Transitions[i].Objective);
+				ts2.Add(q2.Transitions[i].Objective);
+			}
+			foreach(List<MooreState> block in partition)
+            {
+				for (int i = 0; i < ts1.Count && equal; i++)
+				{
+					if (block.Contains(ts1[i]) && !block.Contains(ts2[i]) ||
+							(!block.Contains(ts1[i]) && block.Contains(ts2[i])))
+                    {
+						equal = false;
+                    }
+				}
+			}
+			return equal;
+		}
+
+		public bool PartitionsEquals(List<List<MooreState>> p1, List<List<MooreState>> p2)
+        {
+			bool equal = true;
+
+			if (p1.Count == p2.Count)
+            {
+				for (int i = 0; i < p1.Count && equal; i++)
+				{
+					if (!BlockEquals(p1[i], p2[i]))
+                    {
+						equal = false;
+                    }
+				}
+			}
+            else
+            {
+				equal = false;
+            }
+
+			return equal;
+        }
+
+		public bool BlockEquals(List<MooreState> b1, List<MooreState> b2)
+        {
+			bool equal = true;
+
+			if (b1.Count == b2.Count)
+            {
+				foreach (MooreState state in b1)
+				{
+					if (!b2.Contains(state))
+					{
+						equal = false;
+					}
+				}
+			}
+			else
+            {
+				equal = false;
+            }
+
+			return equal;
+		}
+
 		public bool RemoveState(string toRemove)
 		{
 			MooreState removedState = SearchState(toRemove);
